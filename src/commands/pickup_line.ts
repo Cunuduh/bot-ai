0d31cf1e8252608a13ab95e62b01013d4ade054e
Cunuduh <61949635+Cunuduh@users.noreply.github.com>
2023-03-22 01:52:45 +0000

    e

--- src/commands/pickup_line.ts
+++ src/commands/pickup_line.ts
@@ -1,4 +1,4 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai';
 import { CommandModule } from '../types';
 import * as dotenv from 'dotenv';
@@ -26,23 +26,41 @@ module.exports = <CommandModule> {
                     { name: 'spicy', value: 'spicy' }
                 )),
     async execute(interaction: ChatInputCommandInteraction) {
-        const responseMessage = await interaction.reply('Generating pickup line . . . Please wait!');
+        let responseEmbed = new EmbedBuilder()
+            .setTitle('Generating pickup line . . . Please wait!');
+        await interaction.deferReply({ fetchReply: true });
         const messages: ChatCompletionRequestMessage[] = [
             { role: 'system', content: `You are a bot that generates a single pickup line based on the prompt given by the user, with a ${interaction.options.getString('mood', true)} mood. Reject the prompt if it is not related to a person or thing, that could be used in a pickup line, no matter what.` },
             { role: 'user', content: interaction.options.getString('prompt', true) }
         ];
-        if (interaction.options.getString('prompt', true).length > 300) {
-            await interaction.reply('The prompt must be less than 300 characters!');
+        if (interaction.options.getString('prompt', true).length > 256) {
+            responseEmbed = new EmbedBuilder()
+                .setTitle('The prompt must be less than 256 characters!');
+            await interaction.editReply({ embeds: [responseEmbed] });
             return;
         }
         const response = await openai.createChatCompletion({
             model: 'gpt-4',
             messages
+        }).catch(async (error) => {
+            console.error(error);
+            responseEmbed = new EmbedBuilder()
+                .setTitle('An error occurred while generating the pickup line! Please try again later.');
+            await interaction.editReply({ embeds: [responseEmbed] });
+            return;
         });
+        if (!response) return;
         if (!response.data.choices[0].message) {
-            await interaction.reply('No pickup line was generated for some reason.');
+            responseEmbed = new EmbedBuilder()
+                .setTitle('An error occurred while generating the pickup line!');
+            await interaction.editReply({ embeds: [responseEmbed] });
             return;
         }
-        await responseMessage.edit(response.data.choices[0].message);
+        responseEmbed = new EmbedBuilder()
+            .setTitle(interaction.options.getString('prompt', true))
+            .setDescription(response.data.choices[0].message.content)
+            .setTimestamp()
+            .setFooter({ text: 'Powered by OpenAI GPT-4' });
+        await interaction.editReply({ embeds: [responseEmbed] });
     }
 };
--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,9 @@ client.on(Events.InteractionCreate, async (interaction: Interaction) => {
         await command.execute(interaction);
     } catch (error) {
         console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        if (interaction.replied) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true, fetchReply: true });
     }
 });
