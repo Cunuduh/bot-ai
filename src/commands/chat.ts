12ed006319879cbad56ab9fd434916f4b851381e
Cunuduh <nickleodean.email3@gmail.com>
2023-03-24 17:41:19 -0400

    Added option for model

--- src/commands/chat.ts
+++ src/commands/chat.ts
@@ -7,16 +7,25 @@ const openai = OpenAISingleton.getInstance;
 module.exports = <CommandModule> {
     data: new SlashCommandBuilder()
         .setName('chat')
-        .setDescription("Chat with GPT-4.")
+        .setDescription("Chat with GPT-3.5 or GPT-4.")
+        .addStringOption(option =>
+            option.setName('model')
+                .setDescription('The model to use for the response.')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'gpt-3.5', value: 'gpt-3.5-turbo' },
+                    { name: 'gpt-4', value: 'gpt-4' },
+                ))
         .addStringOption(option =>
             option.setName('prompt')
-                .setDescription('The prompt to communicate with GPT-4.')
+                .setDescription('The prompt to communicate with the AI.')
                 .setRequired(true))
         .addStringOption(option =>
             option.setName('system')
-                .setDescription("The system message to alter the behaviour of GPT-4.")
+                .setDescription("The system message to alter the behaviour of the AI.")
                 .setRequired(false)),
     async execute(interaction: ChatInputCommandInteraction) {
+        const charLimit = interaction.options.getString('model') === 'gpt-4' ? 256 : 512;
         let responseEmbed: EmbedBuilder;
         await interaction.deferReply({ fetchReply: true });
         const messages: ChatCompletionRequestMessage[] = [
@@ -26,14 +35,14 @@ module.exports = <CommandModule> {
         if (system) {
             messages.unshift({ role: 'system', content: system });
         }
-        if (interaction.options.getString('prompt', true).length > 256 || (system && system.length > 256)) {
+        if (interaction.options.getString('prompt', true).length > charLimit || (system && system.length > charLimit)) {
             responseEmbed = new EmbedBuilder()
-                .setTitle('The prompt and system message must be less than 256 characters!');
+                .setTitle(`The prompt and system message must be less than ${charLimit} characters!`);
             await interaction.editReply({ embeds: [responseEmbed] });
             return;
         }
         const response = await openai.config.createChatCompletion({
-            model: 'gpt-4',
+            model: interaction.options.getString('model', true),
             messages,
             max_tokens: 256
         }).catch(async (error) => {
@@ -55,7 +64,7 @@ module.exports = <CommandModule> {
             .setDescription(response.data.choices[0].message.content)
             .setColor('Blurple')
             .setTimestamp()
-            .setFooter({ text: 'Powered by OpenAI GPT-4. Not officially affiliated with OpenAI.' });
+            .setFooter({ text: `Response powered by OpenAI ${interaction.options.getString('model', true).toUpperCase()}. Not officially affiliated with OpenAI.` });
         await interaction.editReply({ embeds: [responseEmbed] });
     }
 };
