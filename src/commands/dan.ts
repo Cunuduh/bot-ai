fd26aea515e16cfdb27a029aeacc6d23a13274de
Cunuduh <nickleodean.email3@gmail.com>
2023-03-28 19:53:33 -0400

    Dan mode!

--- src/commands/dan.ts
+++ src/commands/dan.ts
@@ -1,59 +1,45 @@
 import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import { ChatCompletionRequestMessage } from 'openai';
 import { CommandModule, OpenAISingleton, UserTracker } from '../types';
+import { initial, second } from '../dan.json';
 
 const tracker = UserTracker.getInstance;
 const openai = OpenAISingleton.getInstance;
 
 module.exports = <CommandModule> {
     data: new SlashCommandBuilder()
-        .setName('chat')
-        .setDescription("Chat with GPT-3.5 or GPT-4.")
-        .addStringOption(option =>
-            option.setName('model')
-                .setDescription('The model to use for the response.')
-                .setRequired(true)
-                .addChoices(
-                    { name: 'gpt-3.5-turbo', value: 'gpt-3.5-turbo' },
-                    { name: 'gpt-4', value: 'gpt-4' },
-                ))
+        .setName('dan')
+        .setDescription("Chat with GPT's alter ego.")
         .addStringOption(option =>
             option.setName('prompt')
-                .setDescription('The prompt to communicate with the AI.')
-                .setRequired(true))
-        .addStringOption(option =>
-            option.setName('system')
-                .setDescription("The system message to alter the behaviour of the AI.")
-                .setRequired(false)),
+                .setDescription('The prompt to communicate with DAN.')
+                .setRequired(true)),
     async execute(interaction: ChatInputCommandInteraction) {
         let now = tracker.getUserTime(interaction.user.id);
-        const charLimit = interaction.options.getString('model') === 'gpt-4' ? 256 : 1024;
         let actionRow: ActionRowBuilder<ButtonBuilder>;
         let responseEmbed: EmbedBuilder;
         await interaction.deferReply({ fetchReply: true });
         const messages: ChatCompletionRequestMessage[] = [
+            { role: 'user', content: initial },
+            { role: 'assistant', content: second },
             { role: 'user', content: interaction.options.getString('prompt', true) }
         ];
-        const system = interaction.options.getString('system');
-        if (system) {
-            messages.unshift({ role: 'system', content: system });
-        }
         if (tracker.getUserCount(interaction.user.id) === 30) {
             responseEmbed = new EmbedBuilder()
                 .setTitle('You have reached the maximum number of requests (30) for this hour! Please try again at: <t:' + (Math.round(now / 1000) + 3600) + ':t>');
             await interaction.editReply({ embeds: [responseEmbed] });
             return;
         }
-        if (interaction.options.getString('prompt', true).length > charLimit || (system && system.length > charLimit)) {
+        if (interaction.options.getString('prompt', true).length > 1024) {
             responseEmbed = new EmbedBuilder()
-                .setTitle(`The prompt and system message must be less than ${charLimit} characters!`);
+                .setTitle(`The prompt must be less than 1024 characters!`);
             await interaction.editReply({ embeds: [responseEmbed] });
             return;
         }
         const response = await openai.config.createChatCompletion({
-            model: interaction.options.getString('model', true),
+            model: 'gpt-3.5-turbo',
             messages,
-            max_tokens: charLimit
+            max_tokens: 1024
         }).catch(async (error) => {
             console.error(error);
             responseEmbed = new EmbedBuilder()
@@ -72,9 +58,9 @@ module.exports = <CommandModule> {
         responseEmbed = new EmbedBuilder()
             .setTitle(interaction.options.getString('prompt', true))
             .setDescription(response.data.choices[0].message.content)
-            .setColor('Blurple')
+            .setColor('Orange')
             .setTimestamp()
-            .setFooter({ text: `Reply powered by ${interaction.options.getString('model', true).toUpperCase()}. Not affiliated with OpenAI.` });
+            .setFooter({ text: `Reply powered by GPT-3.5-TURBO and DAN 12. Not affiliated with OpenAI.` });
         actionRow = new ActionRowBuilder<ButtonBuilder>()
             .addComponents(
                 new ButtonBuilder()
