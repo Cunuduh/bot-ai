a70f9b811fdc2e0aa5705cddc6d7718e34bd0259
Cunuduh <nickleodean.email3@gmail.com>
2023-03-31 15:15:10 -0400

    Minor change

--- src/types.ts
+++ src/types.ts
@@ -25,7 +25,7 @@ Censor.disableTier(CensorTier.UserAdded);
 Censor.disableTier(CensorTier.CommonProfanity);
 Censor.disableTier(CensorTier.PossiblyOffensive);
 Censor.disableTier(CensorTier.SexualTerms);
-Censor.setCleanFunction((str: string) => Array.from(str).map(() => '*').join(''));
+Censor.setCleanFunction((str: string) => Array.from(str).map(() => '\*').join(''));
 
 export interface CommandModule {
     data: SlashCommandBuilder;
